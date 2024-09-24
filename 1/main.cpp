44f4623f6c0ace51fa665c8fe7716c050172349a
SannTim <timohaj1@yandex.ru>
2024-09-25 01:27:24 +0300

    modified first task

--- 1/main.cpp
+++ 1/main.cpp
@@ -23,14 +23,16 @@ constexpr int Det(const std::array<std::array<int, N>, N>& matrix, int colMask, 
         }
 
     }
-
     return determinant;
 }
 
 template <int N>
 constexpr int Det(const std::array<std::array<int, N>, N>& matrix) {
     int fullMask = (1 << N) - 1;
-    return Det<N>(matrix, fullMask, N);
+    if (N % 2 == 0)
+        return -Det<N>(matrix, fullMask, N);
+    else 
+        return Det<N>(matrix, fullMask, N);
 }
 
 
@@ -43,7 +45,33 @@ int main() {
 
     constexpr int result = Det<3>(matrix);
     std::cout << result << std::endl;
-
+    constexpr std::array<std::array<int, 4>, 4> matrix4 = { {
+        {0, 1, 2, 4},
+        {1, 2, 3, 7},
+        {2, 3, 7, 1},
+        {1, 3, 0, -5}
+    }};
+    constexpr int resultik = Det<4>(matrix4);
+    std::cout << resultik << std::endl;
+    constexpr std::array<std::array<int, 5>, 5> matrix5 = { {
+        {1, 2, 3, 4, 5},
+        {-1, 0, 2, 5, 3},
+        {1, 2, 2, 4, 6},
+        {-3, 1, -2, 4, 3},
+        {1, 2, 6, 7, -8}
+    }};
+    constexpr int resultik5 = Det<5>(matrix5);
+    std::cout << resultik5 << std::endl;
+    constexpr std::array<std::array<int, 6>, 6> matrix6 = { {
+        {1, 2, 3, 4, 5, 1},
+        {-1, 0, 2, 5, 3, 2},
+        {1, 2, 2, 4, 6, 3},
+        {-3, 1, -2, 4, 3, 4},
+        {1, 2, 6, 7, -8, 5},
+        {1, 2, 2, 4, 6, 6}
+    }};
+    constexpr int resultik6 = Det<6>(matrix6);
+    std::cout << resultik6 << std::endl;
     return 0;
 }
 
