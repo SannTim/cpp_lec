12d2d785186656dfe06855059e57988d2c39e388
SannTim <timohaj1@yandex.ru>
2024-10-01 22:54:38 +0300

    task 3 is done

--- 3/main.cpp
+++ 3/main.cpp
@@ -4,27 +4,26 @@
 
 class A {
 public:
-    static void* operator new(size_t size) {
+    static void * operator new(size_t size) {
         std::cout << "operator new!" << std::endl;
         return ::operator new(size);
     }
-
-    static void operator delete(void* p) {
+    static void operator delete(void *p, size_t size) {
         std::cout << "operator delete!" << std::endl;
-        ::operator delete(p);
+        return ::operator delete(p);
     }
 };
 
-template <class T>
+template <class T, typename P=T>
 struct custom_allocator {
     typedef T value_type;
     custom_allocator() noexcept {}
     template <class U> custom_allocator (const custom_allocator<U>&) noexcept {}
     T* allocate (std::size_t n) {
-        return reinterpret_cast<T*>( ::operator new(n*sizeof(T)));
+        return reinterpret_cast<T*>( P::operator new(n*sizeof(T)));
     }
     void deallocate (T* p, std::size_t n) {
-        ::operator delete(p);
+        P::operator delete(p, sizeof(T) * n);
     }
 
 };
