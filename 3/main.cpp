279badc9e7b0e827ee9aef13c33de2d5c1bd42e7
SannTim <timohaj1@yandex.ru>
2024-10-01 22:55:18 +0300

    modified style

--- 3/main.cpp
+++ 3/main.cpp
@@ -8,6 +8,7 @@ public:
         std::cout << "operator new!" << std::endl;
         return ::operator new(size);
     }
+    
     static void operator delete(void *p, size_t size) {
         std::cout << "operator delete!" << std::endl;
         return ::operator delete(p);
@@ -18,10 +19,13 @@ template <class T, typename P=T>
 struct custom_allocator {
     typedef T value_type;
     custom_allocator() noexcept {}
+
     template <class U> custom_allocator (const custom_allocator<U>&) noexcept {}
+
     T* allocate (std::size_t n) {
         return reinterpret_cast<T*>( P::operator new(n*sizeof(T)));
     }
+
     void deallocate (T* p, std::size_t n) {
         P::operator delete(p, sizeof(T) * n);
     }
