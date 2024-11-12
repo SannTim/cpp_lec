52119cdd2b9ff99c1f2e4580a6a1473684a10bee
Timophy Sannikov <timohaj1@yandex.ru>
2024-11-12 18:37:56 +0300

    added var1 for 5-th task

---
+++ 5/var1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <memory>
+#include <typeinfo>
+
+class Any {
+public:
+    Any() : content(nullptr) {}
+    template<typename T>
+	// make_unique, чтобы за памятью не следить
+    Any(const T& value) : content(std::make_unique<Holder<T>>(value)) {}
+
+    template<typename T>
+    T get() const {
+        if (typeid(T) != content->type()) {
+            throw std::bad_cast();
+        }
+        return static_cast<Holder<T>*>(content.get())->value;
+    }
+
+private:
+    struct BaseHolder {
+        virtual ~BaseHolder() = default;
+        virtual const std::type_info& type() const = 0;
+    };
+    template<typename T>
+    struct Holder : BaseHolder {
+        Holder(const T& value) : value(value) {}
+        const std::type_info& type() const override {
+            return typeid(T);
+        }
+        
+        T value;
+    };
+
+    std::unique_ptr<BaseHolder> content;
+};
+
+
+int main(){
+	Any a(5);
+	std::cout << a.get<int>() << std::endl; // 5
+	try{
+		a.get<std::string>();
+	}
+	catch (std::bad_cast){
+		std::cout << "Cought bad cast\n";
+	}
+}
